94ab67836af3373a4b8633eedc7a0ef5f529904a
marshmannoor <manoorpro@gmail.com>
2026-06-16 09:37:50 +0100

    Added Group Member Names in Localhost

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello from Docker! B9IS121 - Group CA</h1>'
+    return '<h1>Hello from Group 7! B9IS121 - Group CA</h1> ' \
+    '<p> 1. Khawaja Abdul Moiz</p> ' \
+    '<p> 2. Arshman Noor</p> ' \
+    '<p> 3. Hammad Malik</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
