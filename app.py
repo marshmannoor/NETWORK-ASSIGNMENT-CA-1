ae6cd367f9ba22e6849d5c33b6b25dccde9cc05e
marshmannoor <manoorpro@gmail.com>
2026-06-30 10:12:57 +0100

    Add student IDs to homepage

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+﻿from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello from Group 7! B9IS121 - Group CA</h1> ' \
-    '<p> 1. Khawaja Abdul Moiz</p> ' \
-    '<p> 2. Arshman Noor</p> ' \
-    '<p> 3. Hammad Malik</p>'
+    return '''<h1>Hello from Group 7! B9IS121 - Group CA</h1>
+    <p>1. Khawaja Abdul Moiz - 20089410</p>
+    <p>2. Arshman Noor - 20096163</p>
+    <p>3. Hammad Malik - 20073974</p>'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
